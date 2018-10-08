5dc2e58aee8a1627a462fec1e64c373f57d12611
zlh142857 <1056846491@qq.com>
2018-10-08 17:21:54 +0800

    eightth 张立恒 加入加密算法和salt生成工具类 2018/10/8

---
+++ src/main/java/com/hx/config/md5/Md5.java
@@ -0,0 +1,26 @@
+package com.hx.config.md5;/*
+ *//*
+ *@作者:张立恒
+ *@时间:2018/10/8 17:08
+ *@功能:
+ */
+
+import com.hx.model.UserInfo;
+import org.apache.shiro.crypto.RandomNumberGenerator;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+
+public class Md5 {
+    public static String newpassword(String password,String username,String salt){
+        int hashIterations=2;
+        String algorithmName="md5";
+        String newPassword = new SimpleHash(
+                algorithmName,
+                password,
+                ByteSource.Util.bytes(username+salt),
+                hashIterations
+        ).toHex();
+        return newPassword;
+    }
+}
